32d72386b6610ae31c599f1c2047116255ab8768
Dipankar - 1337 Lab <53222413+dipankar-1337lab@users.noreply.github.com>
2020-07-21 17:47:23 +0530

    Changes

--- js/agency.js
+++ js/agency.js
@@ -7,17 +7,23 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+      if($(document).width() > 767){
+        padding_height = $('nav').height();
+      } else {
+        padding_height = $('.navbar .container').height() - $('.navbar-collapse').height();
+      }
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top - padding_height
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
     });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.navbar-fixed-top',
+    offset: parseInt($('.navbar').height()) + 10
 })
 
 // Closes the Responsive Menu on Menu Item Click
@@ -35,3 +41,4 @@ $('div.modal').on('show.bs.modal', function() {
     }
   }
 });
+
